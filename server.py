52927c55dd0c564eda5fbc418cffe36f27b747fb
StephenKwek <sskwek@gmail.com>
2012-12-24 03:56:28 -0800

    init

--- server.py
+++ server.py
@@ -3,11 +3,11 @@ web.config.debug = True
 renderTextPage = web.template.render('textPages/')
 renderCodePage = web.template.render('codePages/')
 
-urls = (
-    '/(.*)', 'index', 'cover', 'resume', 'code',
-    'checkBST', 'divide', 'log2', 'matchPattern', 'nQueen', 'stock'
-)
+textPages = ['index', 'cover', 'resume', 'code']
+codePages = ['checkBST', 'divide', 'log2', 'matchPattern', 'nQueen', 'stock']
+urls = ['/(.*)'] + textPages + codePages
 
+#urls = ('/(.*)', 'index', 'cover', 'resume', 'code', 'checkBST', 'divide', 'log2', 'matchPattern', 'nQueen', 'stock')
 app = web.application(urls, globals())
 
 class index:
@@ -23,6 +23,7 @@ class index:
             "divide" : renderCodePage.divide(),
             "log2" : renderCodePage.log2(),
             "matchPattern" : renderCodePage.matchPattern(),
+            "nQueen" : renderCodePage.nQueen(),
             "stock" : renderCodePage.stock()
         }
         
--- server_unittest.py
+++ server_unittest.py
@@ -1,18 +1,23 @@
-from server import app
+import server 
 import unittest
 
 class ServerTest(unittest.TestCase):
-	def testServer(self):
-		for page in ("index", "cover", "resume", "code"):
-			response = app.request("/" + page)
-			'''
-			print "|" + response.status + "|"
-			print response.status == '200 OK'
-			print "|"+response.data[:30]+"|"
-			print response.data[:30] == '<!DOCTYPE HTML PUBLIC "-//W3C/'
-			'''
-		 	print "|"+response.data[-30:]+"|"
-			print response.data[-30:] == 'TBD</P>\n</OL>\n</BODY>\n</HTML>\n'
+	def testTextPage(self):
+		for page in server.textPages:
+			response = server.app.request("/" + page)
+			self.assertEqual(response.status, '200 OK')
+			self.assertEqual(response.data[:30], '<!DOCTYPE HTML PUBLIC "-//W3C/')
+			self.assertEqual(response.data[-16:], '</BODY>\n</HTML>\n')
+
+		for page in server.codePages:
+			#page = 'nQueen'
+			print page
+			response = server.app.request("/" + page)
+			self.assertEqual(response.status, '200 OK')
+			self.assertEqual(response.data[:30], '<!doctype html 1.1>\n<html>\n<he')
+			self.assertEqual(response.data[-16:], '</body>\n</html>\n')
+	
 
 if __name__ == "__main__":
+	print server.urls
 	unittest.main()
